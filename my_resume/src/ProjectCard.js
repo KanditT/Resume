14b20ae2b484f952356e17aa097f959c3ca2837c
KanditT <kandit.t@kkumail.com>
2024-11-20 00:59:41 +0700

    fix word

--- my_resume/src/ProjectCard.js
+++ my_resume/src/ProjectCard.js
@@ -58,7 +58,7 @@ export default function ExperienceCards() {
             title: "2023 Freshmen Orientation",
             date: "June 2023",
             details: [
-                "Bachelor’s in Computer Engineering, Khon Kaen University",
+                "For: Bachelor’s first year in Computer Engineering, Khon Kaen University",
                 "Prepared slides to guide and support freshmen, providing essential information.",
             ],
             icons: [],
